7f540be3fa2fab83aad1581cb254ed36133f8a1b
zhaozenghao <89393883+zhaozenghao@users.noreply.github.com>
2022-04-01 23:19:11 +0800

    Create VideoUpLoad.java

--- src/main/java/cn/baizhi/util/VideoUpLoad.java
+++ src/main/java/cn/baizhi/util/VideoUpLoad.java
@@ -13,8 +13,8 @@ public class VideoUpLoad {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-beijing.aliyuncs.com";
 // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAI5tQVcKgHXQRSZQevAbX4";
-        String accessKeySecret = "YAQF4gF8abLz9mXF4mW7Fo7HzdBqXH";
+        String accessKeyId = "";
+        String accessKeySecret = "";
 
 // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId,accessKeySecret);
